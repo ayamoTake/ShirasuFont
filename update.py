853dcc97cadacd0752a56c5a6e511b75fcef6f27
ayamoTake <ayamo1013takeuchi@gmail.com>
2025-05-12 19:35:31 +0900

    Update update.py: clear only 1st layer

--- update.py
+++ update.py
@@ -1,4 +1,4 @@
-#!/usr/bin/python3 
+#!/usr/bin/fontforge -script 
 import fontforge
 import os
 
@@ -12,13 +12,13 @@ font.version = "1.0"
 font.os2_weight = 400  # Regular
 
 
-font.appendSFNTName(0x409, 'Family', 'ShirasuFont')
-font.appendSFNTName(0x409, 'SubFamily', 'Regular')
-font.appendSFNTName(0x409, 'Fullname', 'Shirasu Font Regular')
-
-font.appendSFNTName(0x411, 'Family', 'しらすフォント')
-font.appendSFNTName(0x411, 'SubFamily', 'レギュラー')
-font.appendSFNTName(0x411, 'Fullname', 'しらすフォント レギュラー')
+# font.appendSFNTName(0x409, 'Family', 'ShirasuFont')
+# font.appendSFNTName(0x409, 'SubFamily', 'Regular')
+# font.appendSFNTName(0x409, 'Fullname', 'Shirasu Font Regular')
+#
+# font.appendSFNTName(0x411, 'Family', 'しらすフォント')
+# font.appendSFNTName(0x411, 'SubFamily', 'レギュラー')
+# font.appendSFNTName(0x411, 'Fullname', 'しらすフォント レギュラー')
 
 
 SVG_DIR = "glaph_images"
@@ -26,16 +26,19 @@ SVG_DIR = "glaph_images"
 for filename in os.listdir(SVG_DIR):
     if not filename.endswith(".svg"):
         continue
+
     char = os.path.splitext(filename)[0]
     if len(char) != 1:
         continue
 
+    print(f'{filename} loading...')
     codepoint = ord(char)
     svg_path = os.path.join(SVG_DIR, filename)
 
-    glyph = font.createChar(codepoint)
-    glyph.clear()
-    glyph.importOutlines(svg_path)
+    glyph = font.createChar(codepoint)  # 既存があればそのまま取得
+    glyph.clear(1)
+    glyph.importOutlines(svg_path)     # SVGからアウトライン読み込み
+
 
 font_name = "Shirasu.ttf"
 font.generate(font_name)
