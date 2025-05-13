116f2d744a28a786a3c865227c5a614dab6adcb4
ayamoTake <ayamo1013takeuchi@gmail.com>
2025-05-14 00:20:18 +0900

    Fix update script: num bug
    数字に対応する locl と hwid を変更する
    数字と同じ行に数字でない文字が来ても
    フォールバックしない

--- update.py
+++ update.py
@@ -1,6 +1,26 @@
 #!/usr/bin/fontforge -script 
+
 import fontforge
 import os
+
+
+def clone_char(ascii_name, dist_name, font):
+    if dist_name in font:
+        font.removeGlyph(dist_name)
+    dist = font.createChar(-1, dist_name)
+    dist.references = [(ascii_name, (1, 0, 0, 1, 0, 0,))]
+    dist.width = 1024
+
+
+def set_local_and_hwid(font, num):
+    ascii_name = ["zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine"][num]
+
+    locl_name = f"{ascii_name}.locl"
+    clone_char(ascii_name, locl_name, font)
+
+    hwid_name = f"{ascii_name}.hwid"
+    clone_char(ascii_name, hwid_name, font)
+
 
 font = fontforge.open("original.ttf")
 font.encoding = "UnicodeFull"
@@ -12,13 +32,13 @@ font.version = "1.0"
 font.os2_weight = 400  # Regular
 
 
-# font.appendSFNTName(0x409, 'Family', 'ShirasuFont')
-# font.appendSFNTName(0x409, 'SubFamily', 'Regular')
-# font.appendSFNTName(0x409, 'Fullname', 'Shirasu Font Regular')
-#
-# font.appendSFNTName(0x411, 'Family', 'しらすフォント')
-# font.appendSFNTName(0x411, 'SubFamily', 'レギュラー')
-# font.appendSFNTName(0x411, 'Fullname', 'しらすフォント レギュラー')
+font.appendSFNTName(0x409, 'Family', 'ShirasuFont')
+font.appendSFNTName(0x409, 'SubFamily', 'Regular')
+font.appendSFNTName(0x409, 'Fullname', 'Shirasu Font Regular')
+
+font.appendSFNTName(0x411, 'Family', 'しらすフォント')
+font.appendSFNTName(0x411, 'SubFamily', 'レギュラー')
+font.appendSFNTName(0x411, 'Fullname', 'しらすフォント レギュラー')
 
 
 SVG_DIR = "glaph_images"
@@ -38,8 +58,18 @@ for filename in os.listdir(SVG_DIR):
     glyph = font.createChar(codepoint)  # 既存があればそのまま取得
     glyph.clear(1)
     glyph.importOutlines(svg_path)     # SVGからアウトライン読み込み
+    glyph.width = 1024
+
+    if '0' <= char <= '9':
+        # 全角数字の処理.
+        glyph = font.createChar(0xFF10 + int(char))
+        glyph.clear(1)
+        glyph.importOutlines(svg_path)     # SVGからアウトライン読み込み
+        glyph.width = 1024
+
+        set_local_and_hwid(font, int(char))
 
 
 font_name = "Shirasu.ttf"
-font.generate(font_name)
+font.generate(font_name, flags=("opentype"))
 print(font_name, " created.")
